82130f3e9af711c06c28be84b46ed6526758d6b8
aleksejbratenkov <oleksii.bratenkov_ges@omio.com>
2025-02-18 22:13:57 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # zopls-test
+
+Implement a class with a method inside that will retrieve categories and products, convert them into ProductSummary,
+and return Map<String, List<ProductSummary>>. (key is a category name and value is a list of ProductSummary) Use DI
--- src/test/java/com/zooplus/ProductProcessorTest.java
+++ src/test/java/com/zooplus/ProductProcessorTest.java
@@ -1,5 +1,8 @@
 package com.zooplus;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -9,6 +12,8 @@ class ProductProcessorTest {
 
     @Test
     void shouldReturnProcessedProducts() {
+        var result = new HashMap<>();
 
+        assertThat(result).isEmpty();
     }
 }
